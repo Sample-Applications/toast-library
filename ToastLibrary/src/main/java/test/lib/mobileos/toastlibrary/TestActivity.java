a8f5b80796ce38c7654a3ba77bd5045399fe524d
kenprogrammer <kenprogrammer@gmail.com>
2022-06-21 17:40:47 +0300

    Navigate to second activity on module

--- ToastLibrary/src/main/java/test/lib/mobileos/toastlibrary/TestActivity.java
+++ ToastLibrary/src/main/java/test/lib/mobileos/toastlibrary/TestActivity.java
@@ -2,13 +2,23 @@ package test.lib.mobileos.toastlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
+    Button btnNextLibActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        btnNextLibActivity=findViewById(R.id.btnNextLibActivity);
+        btnNextLibActivity.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), TestActivity2.class);
+            startActivity(intent);
+        });
     }
 }
